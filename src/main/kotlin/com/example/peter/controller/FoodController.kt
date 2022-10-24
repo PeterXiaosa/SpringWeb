971ab9d0de098f91922a145cd4a32af77faad63c
Peter_Fu <13970643306fhc>
2022-10-24 21:09:08 +0800

    1. 修改。

--- src/main/kotlin/com/example/peter/controller/FoodController.kt
+++ src/main/kotlin/com/example/peter/controller/FoodController.kt
@@ -35,64 +35,34 @@ class FoodController {
         val res = JSONObject()
         val listArray = JSONArray()
         val category1Json = JSONObject()
-        category1Json.put("name", "感冒类")
+        category1Json.put("name", "一部")
         val category1FoodArray = JSONArray()
         val category1Food1Json = JSONObject()
-        category1Food1Json["id"] = 1
-        category1Food1Json["image_url"] = "../../images/duola.png"
-        category1Food1Json.put("name", "止咳糖浆1")
+        category1Food1Json["id"] = 0
+        category1Food1Json["image_url"] = "../../images/10袋小儿咽扁颗粒.png"
+        category1Food1Json.put("name", "10袋小儿咽扁颗粒")
         category1Food1Json.put("price", 10)
         category1FoodArray.add(category1Food1Json)
         val category1Food2Json = JSONObject()
-        category1Food2Json.put("id", 2)
-        category1Food2Json["image_url"] = "../../images/duola.png"
-        category1Food2Json.put("name", "止咳糖浆2")
+        category1Food2Json.put("id", 1)
+        category1Food2Json["image_url"] = "../../images/130片蚝贝钙咀嚼片小盒.png"
+        category1Food2Json.put("name", "130片蚝贝钙咀嚼片小盒")
         category1Food2Json.put("price", 20)
         category1FoodArray.add(category1Food2Json)
-        val category1Food3Json = JSONObject()
-        category1Food3Json.put("id", 3)
-        category1Food3Json["image_url"] = "../../images/duola.png"
-        category1Food3Json.put("name", "止咳糖浆3")
-        category1Food3Json.put("price", 20)
-        category1FoodArray.add(category1Food3Json)
-        val category1Food4Json = JSONObject()
-        category1Food4Json.put("id", 4)
-        category1Food4Json["image_url"] = "../../images/duola.png"
-        category1Food4Json.put("name", "止咳糖浆4")
-        category1Food4Json.put("price", 20)
-        category1FoodArray.add(category1Food4Json)
-        val category1Food5Json = JSONObject()
-        category1Food5Json.put("id", 5)
-        category1Food5Json["image_url"] = "../../images/duola.png"
-        category1Food5Json.put("name", "止咳糖浆5")
-        category1Food5Json.put("price", 20)
-        category1FoodArray.add(category1Food5Json)
-        val category1Food6Json = JSONObject()
-        category1Food6Json.put("id", 6)
-        category1Food6Json["image_url"] = "../../images/duola.png"
-        category1Food6Json.put("name", "止咳糖浆6")
-        category1Food6Json.put("price", 20)
-        category1FoodArray.add(category1Food6Json)
-        val category1Food7Json = JSONObject()
-        category1Food7Json.put("id", 7)
-        category1Food7Json["image_url"] = "../../images/duola.png"
-        category1Food7Json.put("name", "止咳糖浆7")
-        category1Food7Json.put("price", 20)
-        category1FoodArray.add(category1Food7Json)
         category1Json.put("food", category1FoodArray)
         val category2Json = JSONObject()
-        category2Json.put("name", "咳嗽类")
+        category2Json.put("name", "二部")
         val category2FoodArray = JSONArray()
         val category2Food2Json = JSONObject()
-        category2Food2Json.put("id", 8)
-        category2Food2Json["image_url"] = "../../images/duola.png"
-        category2Food2Json.put("name", "咳嗽糖浆1")
+        category2Food2Json.put("id", 0)
+        category2Food2Json["image_url"] = "../../images/10片阿莫西林分散片.png"
+        category2Food2Json.put("name", "10片阿莫西林分散片")
         category2Food2Json.put("price", 30)
         category2FoodArray.add(category2Food2Json)
         val category2Food1Json = JSONObject()
-        category2Food1Json.put("id", 9)
-        category2Food1Json["image_url"] = "../../images/duola.png"
-        category2Food1Json.put("name", "咳嗽糖浆2")
+        category2Food1Json.put("id", 1)
+        category2Food1Json["image_url"] = "../../images/12ml萘敏维滴眼液a.png"
+        category2Food1Json.put("name", "12ml萘敏维滴眼液a")
         category2Food1Json.put("price", 40)
         category2FoodArray.add(category2Food1Json)
         category2Json.put("food", category2FoodArray)
