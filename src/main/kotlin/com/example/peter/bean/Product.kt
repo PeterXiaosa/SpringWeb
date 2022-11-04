24e12de9f56387c09263a93397276f4ef59ef771
PeterXiaosa <fhcxiaosa@hotmail.com>
2022-11-04 10:42:06 +0800

    1. 修改bean。

--- src/main/kotlin/com/example/peter/bean/Product.kt
+++ src/main/kotlin/com/example/peter/bean/Product.kt
@@ -3,4 +3,4 @@ package com.example.peter.bean
 import java.math.BigDecimal
 
 data class Product(var productId:Int? = 0, var name:String? ="", var price:BigDecimal?= BigDecimal.ZERO, var imageUrl:String?="",
-                    var stockNum:Int? = 0)
+                    var stockNum:Int? = 0, var id:Int = 0, var productType:String? = "")
